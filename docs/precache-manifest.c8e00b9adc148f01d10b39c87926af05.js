b80eeab39e7972f4674b879cdc8fc3fb440ffdcc
ignaciosps <ignaciospst@gmail.com>
2020-08-05 20:18:16 -0300

    Docs agregado

---
+++ docs/precache-manifest.c8e00b9adc148f01d10b39c87926af05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb31e3b5bb938b20a0fde0293f61e5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d491c5a52bd6c85bbaea",
+    "url": "/static/css/main.4b4300ef.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d491c5a52bd6c85bbaea",
+    "url": "/static/js/main.f7d9bdf7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
